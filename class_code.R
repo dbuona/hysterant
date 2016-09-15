b151f3046153e0e804d07fe148baf86ba7e232b6
dbuona <dbuonaiuto@g.harvard.edu>
2016-09-15 13:19:54 -0400

    Recoding for class
    Still mostly playing around

---
+++ class_code.R
@@ -0,0 +1,10 @@
+setwd("~/Documents/hyst_work_direct")
+phen<- read.csv(file="hf_data.csv",header=T)
+phen
+head(phen)
+phen$hyst<-(phen$fopn.jd)-(phen$bb.jd)
+phen
+phen$class<-(phen$hyst)
+phen
+head(phen)
+transform<-
