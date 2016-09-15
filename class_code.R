6cc466293f124b4e84cb58e66db90bfec4113eb2
dbuona <dbuonaiuto@g.harvard.edu>
2016-09-15 16:06:49 -0400

    Created classification assignment/species

--- class_code.R
+++ class_code.R
@@ -5,7 +5,17 @@ head(phen)
 phen$hyst<-(phen$fopn.jd)-(phen$bb.jd)
 phen
 phen$class<-(phen$hyst)
-phen
 head(phen)
-transform<-
-  ##now I am adding to see how git works
+library(car)
+phen$class<-recode(phen$class,"0:hi='No'")
+phen
+range(phen$class, na.rm=T)
+phen$class<-recode(phen$class,"-38:-1='Yes';0:79='No'")
+phen$class<-recode(phen$class, "'Yes'=1;'No'=0")
+reduced<-data.frame(phen$species,phen$class)
+library(dplyr)
+tbl_df(reduced)
+View(reduced)
+group_by(phen,species)
+
+
